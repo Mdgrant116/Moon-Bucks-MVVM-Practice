67fdd946cf2afce5a066447f135b83423402e476
Michael Grant <mdgrant116@gmail.com>
2019-12-10 15:27:14 -0500

    Initial views setup, no design yet.

--- Moon Bucks/Controlelrs/AddOrderViewController.swift
+++ Moon Bucks/Controlelrs/AddOrderViewController.swift
@@ -10,8 +10,53 @@ import Foundation
 import UIKit
 
 class AddOrderViewController: UIViewController {
+   
+    private var vm = AddCoffeeOrderViewModel()
     
     
+    @IBOutlet weak var tableView: UITableView!
+    private var coffeeSizesSegmentedController: UISegmentedControl!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupUI()
+    }
+    
+    
+    private func setupUI() {
+        
+        self.coffeeSizesSegmentedController = UISegmentedControl(items: self.vm.sizes)
+        self.coffeeSizesSegmentedController.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.view.addSubview(coffeeSizesSegmentedController)
+        self.coffeeSizesSegmentedController.topAnchor.constraint(equalTo: self.tableView.bottomAnchor, constant: 20).isActive = true
+        self.coffeeSizesSegmentedController.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        
+    }
+    @IBAction func closeButtonTapped(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
+
+
+extension AddOrderViewController: UITableViewDelegate, UITableViewDataSource {
+   
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+         return self.vm.types.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CoffeeType", for: indexPath)
+        
+        cell.textLabel?.text = self.vm.types[indexPath.row]
+        
+        return cell
+    }
     
     
     
---
+++ Moon Bucks/View Models/AddCoffeeOrderViewModel.swift
@@ -0,0 +1,28 @@
+//
+//  AddCoffeeOrderViewModel.swift
+//  Moon Bucks
+//
+//  Created by Michael Grant on 12/10/19.
+//  Copyright © 2019 Michael Grant. All rights reserved.
+//
+
+import Foundation
+
+struct AddCoffeeOrderViewModel {
+    
+    var name: String?
+    var email: String?
+    
+    var types: [String] {
+        
+        return CoffeeType.allCases.map { $0.rawValue.capitalized }
+        
+    }
+    
+    var sizes: [String] {
+        
+        return CoffeeSize.allCases.map { $0.rawValue.capitalized }
+        
+    }
+    
+}
